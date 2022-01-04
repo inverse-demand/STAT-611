3659e81d264ebc3d72dd1a8ed62d59115172dea8
Nathanial Watanabe <shoyawatanabe@gmail.com>
2022-01-04 01:15:33 -0900

    Week 2 updated

---
+++ WEEK 2.R
@@ -0,0 +1,62 @@
+# 1. Please Create a new variable, called "Feedback" which will contain
+# the numbers 3,4,5,3,2,3,2,1,4,5,3,2,3,4,5,2,1,3,4,5,1,2,3,2,3,4,1,4,5,2,3
+
+Feedback <- c(3,4,5,3,2,3,2,1,4,5,3,2,3,4,5,2,1,3,4,5,1,2,3,2,3,4,1,4,5,2,3)
+
+# 1)Please check the mode and class of feedback
+mode(Feedback)
+class(Feedback)
+
+# 2)Please extract the 10th number
+
+Feedback[10]
+
+# 3.
+# Create the matrix below
+# [,1] [,2] [,3]
+# [1,] 7 -7 7
+# [2,] 7 -7 7
+# [3,] 7 -7 7
+
+a.matrix <- matrix(data = c(7,-7,7,
+                            7,-7,7,
+                            7,-7,7),
+                   nrow = 3,
+                   ncol = 3,
+                   byrow = TRUE)
+a.matrix
+# 1) Name columns and rows as " row 1", "row 2", "row 3","col 1", "col 2", "col 3"
+
+
+# 2)Extract the second and the third column
+a.matrix[,2:3]
+
+# 3)Extract the third and second row
+a.matrix[1:3]
+
+# 4)get the diagonal data
+diag(a.matrix)
+
+# 5)Please delete the second row
+# 6)Based on question 6, please overwrite the second row of the matrix with c(1,2,3)
+
+
+# 5. Please create an array (with 3*3*2 dimension) shown below, name it as myarray
+
+# , , Matrix1
+# 
+# COL1 COL2 COL3
+# ROW1 5 10 13
+# ROW2 9 11 14
+# ROW3 3 12 15
+
+# , , Matrix2
+
+# COL1 COL2 COL3
+# ROW1 5 10 13
+# ROW2 9 11 14
+# ROW3 3 12 15
+
+# 1)please extract the second row of the second layer of myarray
+# 2)Please extract the first rows of both layers of myarray
+# 3)please extract the third and first element from the second row of the second layer of myarrary
